52b8bd85d19786dfda3ff23315c995fc2f43301b
Eleos <Peaceaanu247@gmail.com>
2022-02-12 14:39:16 +0100

    files changed into php

---
+++ includes/sidebar.php
@@ -0,0 +1,44 @@
+<header class="header" id="header">
+      <div class="header_toggle">
+        <i class="bx bx-menu" id="header-toggle"></i>
+      </div>
+      <div class="header_img">
+        <img src="images/avatar.JPG" alt="" />
+      </div>
+    </header>
+    <div class="l-navbar" id="nav-bar">
+      <nav class="nav">
+        <div>
+          <a href="index.php" class="nav_logo">
+            <i class="bx bx-layer nav_logo-icon"></i>
+            <span class="nav_logo-name">Ed Bank</span>
+          </a>
+          <div class="nav_list">
+            <a href="index.php" class="nav_link active">
+              <i class="bx bx-grid-alt nav_icon"></i>
+              <span class="nav_name">Dashboard</span>
+            </a>
+            <a href="all-customers.php" class="nav_link">
+              <i class="bx bx-user nav_icon"></i>
+              <span class="nav_name">All Customers</span>
+            </a>
+            <a href="transfers.php" class="nav_link">
+              <i class="bx bx-send nav_icon"></i>
+              <span class="nav_name">Send Money</span>
+            </a>
+            <a href="add-customers.php" class="nav_link">
+              <i class="bx bx-user-plus nav_icon"></i>
+              <span class="nav_name">Add New Customers</span>
+            </a>
+            <a href="delete-customers.php" class="nav_link">
+              <i class="bx bx-eraser nav_icon"></i>
+              <span class="nav_name">Delete Customers</span>
+            </a>
+          </div>
+        </div>
+        <a href="#" class="nav_link">
+          <i class="bx bxs-copyright nav_icon"></i>
+          <span class="nav_name">Eleos 2020</span>
+        </a>
+      </nav>
+    </div>
--- single-customer.php
+++ single-customer.php
@@ -27,50 +27,8 @@
   </head>
 
   <body id="body-pd">
-    <header class="header" id="header">
-      <div class="header_toggle">
-        <i class="bx bx-menu" id="header-toggle"></i>
-      </div>
-      <div class="header_img">
-        <img src="images/avatar.JPG" alt="" />
-      </div>
-    </header>
-    <div class="l-navbar" id="nav-bar">
-      <nav class="nav">
-        <div>
-          <a href="index.html" class="nav_logo">
-            <i class="bx bx-layer nav_logo-icon"></i>
-            <span class="nav_logo-name">Ed Bank</span>
-          </a>
-          <div class="nav_list">
-            <a href="index.html" class="nav_link">
-              <i class="bx bx-grid-alt nav_icon"></i>
-              <span class="nav_name">Dashboard</span>
-            </a>
-            <a href="all-customers.html" class="nav_link">
-              <i class="bx bx-user nav_icon"></i>
-              <span class="nav_name active">All Customers</span>
-            </a>
-            <a href="transfers.html" class="nav_link">
-              <i class="bx bx-send nav_icon"></i>
-              <span class="nav_name">Send Money</span>
-            </a>
-            <a href="add-customers.html" class="nav_link">
-              <i class="bx bx-user-plus nav_icon"></i>
-              <span class="nav_name">Add New Customers</span>
-            </a>
-            <a href="delete-customers.html" class="nav_link">
-              <i class="bx bx-eraser nav_icon"></i>
-              <span class="nav_name">Delete Customers</span>
-            </a>
-          </div>
-        </div>
-        <a href="#" class="nav_link">
-          <i class="bx bxs-copyright nav_icon"></i>
-          <span class="nav_name">Eleos 2020</span>
-        </a>
-      </nav>
-    </div>
+   <!-- Sidebar starts -->
+    <?php include('includes/sidebar.php'); ?>
 
     <!--Container Main start-->
     <div class="height-100">
